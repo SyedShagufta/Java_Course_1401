d748b26ecb9c03064089a16a679e86b0ff45cf10
SyedShagufta <sweetyvox123@gmail.com>
2025-09-30 15:00:26 +0530

    Patterns using loops

---
+++ com/learning/basics_01/PatternsUsingLoops.java
@@ -0,0 +1,29 @@
+package com.learning.basics_01;
+
+public class PatternsUsingLoops {
+
+    public static void main(String[] args) {
+
+        for(int i=1; i<=10; i++){
+            for(int j=1; j<i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i=10; i>1; i--){
+            for(int j=1; j<i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+
+    }
+
+}
