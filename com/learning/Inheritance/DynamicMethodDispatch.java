c4035d1eebed2a36d54a3c8dff19816c94036bf5
SyedShagufta <sweetyvox123@gmail.com>
2025-10-11 18:34:42 +0530

    Dynamic Method Dispatch in Java

---
+++ com/learning/Inheritance/DynamicMethodDispatch.java
@@ -0,0 +1,50 @@
+package com.learning.Inheritance;
+
+class Fruit{
+    public void eat(){
+        System.out.println("A fruit is eatable");
+
+    }
+}
+
+class PineApple extends Fruit{
+
+    @Override
+    public void eat(){
+        super.eat();
+        System.out.println("This is pineapple fruit");
+        System.out.println("---".repeat(30));
+    }
+
+}
+
+class Strawberry extends Fruit{
+    @Override
+    public void eat(){
+        super.eat();
+        System.out.println("This is strawberry fruit");
+        System.out.println("---".repeat(30));
+    }
+}
+
+public class DynamicMethodDispatch {
+
+    public static void main(String[] args) {
+
+        PineApple pineApple = new PineApple();
+        Strawberry strawberry = new Strawberry();
+
+        // Let's take the reference
+        Fruit fruit;
+
+        fruit = pineApple;
+        fruit.eat();
+
+        fruit = strawberry;
+        fruit.eat();
+
+
+
+    }
+
+}
